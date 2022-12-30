8df895e2370af548ba7972a539efaf070043b35f
William Hatch <wkhatch@me.com>
2022-12-30 00:01:52 -0500

    gah later version of the provider do not support helm_repo data source; rolling back

--- deploy-chart.tf
+++ deploy-chart.tf
@@ -13,21 +13,12 @@
 
 # }
 
-data "helm_repository" "mosquitto" {
-  name = "mosquitto"
-  url = "https://storage.googleapis.com/t3n-helm-charts"
-}
-
 resource "helm_release" "mosquitto" {
   name             = "mosquitto"
-  repository            = data.helm_repository.mosquitto.metadata[0].name
+  repository            = "https://storage.googleapis.com/t3n-helm-charts"
   # chart = "t3n/mosquitto"
   chart = "mosquitto"
   version = "2.4.1"
   namespace        = "mosquitto"
   create_namespace = true
-  # values = [
-
-  #   templatefile("./values.yml", {environment = var.environment, svcAcctName = ${local.svcAcctName} serviceName = ${local.serviceName}, storageClassName = kubernetes_storage_class_v1.ebs_cs_as_sc.metadata.[0].name, })
-  # ]
 }
