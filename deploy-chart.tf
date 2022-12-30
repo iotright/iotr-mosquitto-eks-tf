c6aea47addbe96433bdf6955a0812454e30b9967
William Hatch <wkhatch@me.com>
2022-12-29 23:40:37 -0500

    added repository, and modified deployment

--- deploy-chart.tf
+++ deploy-chart.tf
@@ -13,41 +13,21 @@
 
 # }
 
+data "helm_repository" "mosquitto" {
+  name = "mosquitto"
+  url = "https://storage.googleapis.com/t3n-helm-charts"
+}
+
 resource "helm_release" "mosquitto" {
   name             = "mosquitto"
-  # repository            = "https://storage.googleapis.com/t3n-helm-charts"
+  repository            = data.helm_repository.mosquitto.metadata[0].name
   # chart = "t3n/mosquitto"
-  chart = "./Chart.yaml"
+  chart = "mosquitto"
   version = "2.4.1"
   namespace        = "mosquitto"
   create_namespace = true
-  values = [
-    # ${yamlencode({
-    #   "image.pullPolicy": "always",
-
-    # })}
-    templatefile("./values.yml", {environment = var.environment, svcAcctName = ${local.svcAcctName} serviceName = ${local.serviceName}, storageClassName = kubernetes_storage_class_v1.ebs_cs_as_sc.metadata.[0].name, })
-  ]
-}
-
-# honeycomb.yml
-
-# agent:
-#   watchers:
-#     - labelSelector: "app=my-app"
-#       namespace: my-namespace
-#       dataset: "${env}"
-#       parser:
-#         name: nginx
-#         dataset: "${env}"
-#         options:
-#           log_format: "blah"
-
-# ${yamlencode({
-#   "backends": [for addr in ip_addrs : "${addr}:${port}"],
-# })}
-
-# templatefile 
   # values = [
-  #   templatefile("modules/kubernetes/helm/honeycomb.yml", { env = "${var.env}" })
+
+  #   templatefile("./values.yml", {environment = var.environment, svcAcctName = ${local.svcAcctName} serviceName = ${local.serviceName}, storageClassName = kubernetes_storage_class_v1.ebs_cs_as_sc.metadata.[0].name, })
   # ]
+}
