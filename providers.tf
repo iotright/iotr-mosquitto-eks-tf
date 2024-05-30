3740486939c19db06e9085117103855d00feb2d1
William Hatch <wkhatch@me.com>
2024-05-30 16:30:16 -0400

    remove helm from providers

--- providers.tf
+++ providers.tf
@@ -37,13 +37,13 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 # HELM Provider
-provider "helm" {
-  kubernetes {
-    host                   = data.terraform_remote_state.core.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.core.outputs.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.terraform_remote_state.core.outputs.cluster_endpoint
+#     cluster_ca_certificate = base64decode(data.terraform_remote_state.core.outputs.cluster_certificate_authority_data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
 
 
 # Kubernetes provider
