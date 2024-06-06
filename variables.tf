229313d4ffe966af5331a32f7976d670484acf44
William Hatch <wkhatch@me.com>
2024-06-06 11:58:29 -0400

    dyanmic var to determie which addons repo based on env

--- variables.tf
+++ variables.tf
@@ -8,6 +8,16 @@ variable "workspace_for_environment" {
   }
 }
 
+variable "addons_workspace_for_environment" {
+  type = map(any)
+  default = {
+    development = "iotr-eksaddons-tf-development"
+    qa          = "iotr-eksaddons-tf-qa"
+    staging     = "iotr-eksaddons-tf-staging"
+    production  = "iotr-eksaddons-tf-production"
+  }
+}
+
 locals {
   serviceName = "${var.environment}-mosquitto"
   svcAcctName = "${var.environment}-mosquitto-sa"
