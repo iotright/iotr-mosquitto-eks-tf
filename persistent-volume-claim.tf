69a7357b05401946fd58b7ec8c5dcba0c995c1f8
William Hatch <wkhatch@me.com>
2024-06-06 14:17:29 -0400

    try creating a different storage class and using that

--- persistent-volume-claim.tf
+++ persistent-volume-claim.tf
@@ -1,3 +1,12 @@
+resource "kubernetes_storage_class_v1" "ebs_mosquitto_sc" {
+  metadata {
+    name = "ebs-mosquitto-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "WaitForFirstConsumer"
+}
+
+
 resource "kubernetes_persistent_volume_claim_v1" "mq_pv_claim" {
   metadata {
     name = "mq-pv-claim"
@@ -8,7 +17,7 @@ resource "kubernetes_persistent_volume_claim_v1" "mq_pv_claim" {
 
   spec {
     access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "ebs-sc"
+    storage_class_name = kubernetes_storage_class_v1.ebs_mosquitto_sc.metadata.0.name
     resources {
       requests = {
         storage = "2Gi"
