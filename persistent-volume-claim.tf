15985eef94c31cd6f83c9f0a136f349f669fcbe2
William Hatch <wkhatch@me.com>
2024-06-07 18:24:07 -0400

    maybe force the deployment first

--- persistent-volume-claim.tf
+++ persistent-volume-claim.tf
@@ -7,21 +7,21 @@ resource "kubernetes_storage_class_v1" "ebs_mosquitto_sc" {
 }
 
 
-resource "kubernetes_persistent_volume_claim_v1" "mq_pv_claim" {
-  metadata {
-    name = "mq-pv-claim"
-    labels = {
-      app = "mosquitto"
-    }
-  }
+# resource "kubernetes_persistent_volume_claim_v1" "mq_pv_claim" {
+#   metadata {
+#     name = "mq-pv-claim"
+#     labels = {
+#       app = "mosquitto"
+#     }
+#   }
 
-  spec {
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = kubernetes_storage_class_v1.ebs_mosquitto_sc.metadata.0.name
-    resources {
-      requests = {
-        storage = "2Gi"
-      }
-    }
-  }
-}
+#   spec {
+#     access_modes       = ["ReadWriteOnce"]
+#     storage_class_name = kubernetes_storage_class_v1.ebs_mosquitto_sc.metadata.0.name
+#     resources {
+#       requests = {
+#         storage = "2Gi"
+#       }
+#     }
+#   }
+# }
