d71a1f0924e94c43a9593b7780fd7c233f0d46d5
William Hatch <wkhatch@me.com>
2024-06-13 07:15:23 -0400

    and one more time
    post cleaning up resources in add ons workspace and repo

--- persistent-volume-claim.tf
+++ persistent-volume-claim.tf
@@ -2,8 +2,8 @@ resource "kubernetes_storage_class_v1" "ebs_mosquitto_sc" {
   metadata {
     name = "ebs-mosquitto-sc"
   }
-  #   storage_provisioner = "ebs.csi.aws.com"
-  storage_provisioner = "kubernetes.io/aws-ebs"
+  storage_provisioner = "ebs.csi.aws.com"
+  #   storage_provisioner = "kubernetes.io/aws-ebs"
   volume_binding_mode = "WaitForFirstConsumer"
 }
 
@@ -17,9 +17,9 @@ resource "kubernetes_persistent_volume_claim_v1" "mq_pv_claim" {
   }
 
   spec {
-    access_modes = ["ReadWriteOnce"]
-    # storage_class_name = kubernetes_storage_class_v1.ebs_mosquitto_sc.metadata.0.name
-    storage_class_name = "ebs-mosquitto-sc"
+    access_modes       = ["ReadWriteOnce"]
+    storage_class_name = kubernetes_storage_class_v1.ebs_mosquitto_sc.metadata.0.name
+    # storage_class_name = "ebs-mosquitto-sc"
     resources {
       requests = {
         storage = "2Gi"
